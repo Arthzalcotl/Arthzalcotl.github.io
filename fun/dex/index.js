b4bf0c3794990c34ff066b7f4c0e55b3d97e732e
maventoto <toto@maven.co.id>
2020-03-06 13:36:19 +0700

    testing sort

--- fun/dex/index.js
+++ fun/dex/index.js
@@ -1,4 +1,6 @@
 $(document).ready((e) => {
+	listSort(data)
+
 	let data_str = ''
 	for(item of data) {
 		data_str += '<div class="col-sm-3">';
@@ -7,3 +9,17 @@ $(document).ready((e) => {
 	}
 	$('.container .row').html(data_str);
 });
+
+function listSort(list_data) {
+	list_data.sort(function(a, b) {
+    var text_a = a.group.toLowerCase();
+    var text_b = b.group.toLowerCase();
+    if(text_a < text_b) return -1;
+		if(text_a > text_b) return 1;
+		
+    if(a.name < b.name) return -1;
+		if(a.name > b.name) return 1;
+		
+    return 0;
+  });
+}
